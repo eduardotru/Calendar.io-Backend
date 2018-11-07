4a5842873b5ff70632f1d4df4dca7513f7cc3a12
Esteban Arocha Ortuno <estebanarochaortuno@Estebans-MacBook-Pro-2.local>
2018-11-06 22:41:36 -0600

    Models and User API

---
+++ db/migrate/20181107030315_create_events.rb
@@ -0,0 +1,12 @@
+class CreateEvents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :events do |t|
+      t.integer :duration
+      t.datetime :dueDate
+      t.boolean :static
+      t.integer :owner
+
+      t.timestamps
+    end
+  end
+end
